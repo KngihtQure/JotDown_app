05c6026be0803f67fd540636d84dea6c2b1e6b01
znallaer <lanzcoreal@gmail.com>
2026-06-28 09:38:18 +0800

    updated the activity_create_note to be constant. which fixes the issue of layout compressing when an image is added.

--- app/src/main/java/com/example/jotdown_app/MainActivity.kt
+++ app/src/main/java/com/example/jotdown_app/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Setup RecyclerView
+
         adapter = NoteAdapter(emptyList())
         binding.rvNotes.layoutManager = LinearLayoutManager(this)
         binding.rvNotes.adapter = adapter
